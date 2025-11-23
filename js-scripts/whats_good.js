d9cfa4c0de587fa6cfe4cfa90dedfea01f08c25c
khbsd <khb.dev@gmail.com>
2025-11-23 00:13:36 -0600

    hmm

--- js-scripts/whats_good.js
+++ js-scripts/whats_good.js
@@ -1,1 +1,8 @@
-console.log("whats mcfuckin good cunts");
+import { WriteFile } from 'fs'
+import { path } from 'path'
+
+let string = "console.log('whats mcfuckin good cunts');";
+
+WriteFile(path.normalize("./logging.js"), string, (err) => {
+    if (err) throw err;
+})

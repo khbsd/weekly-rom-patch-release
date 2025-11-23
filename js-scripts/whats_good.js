61410ad90c26986e31afde1a36866d7c0001eff7
khbsd <khb.dev@gmail.com>
2025-11-23 00:19:16 -0600

    testing take whatever

--- js-scripts/whats_good.js
+++ js-scripts/whats_good.js
@@ -4,6 +4,6 @@ import path from 'path'
 let string = "console.log('whats mcfuckin good cunts');";
 const data = new Uint8Array(Buffer.from(string));
 
-writeFile(path.normalize("./logging.js"), data, (err) => {
+writeFile(path.normalize("./js-scripts/logging.js"), data, (err) => {
     if (err) throw err;
 })

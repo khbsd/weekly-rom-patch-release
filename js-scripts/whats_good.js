0c8a220779b3dd3b85a8fc679c73779f243a1d9e
khbsd <khb.dev@gmail.com>
2025-11-23 00:14:34 -0600

    more.js

--- js-scripts/whats_good.js
+++ js-scripts/whats_good.js
@@ -1,8 +1,9 @@
-import { WriteFile } from 'fs'
+import { WriteFile } from 'node:fs'
 import { path } from 'path'
 
 let string = "console.log('whats mcfuckin good cunts');";
+const data = new Uint8Array(Buffer.from(string));
 
-WriteFile(path.normalize("./logging.js"), string, (err) => {
+WriteFile(path.normalize("./logging.js"), data, (err) => {
     if (err) throw err;
 })

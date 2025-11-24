bbbc96b462088ddf2bfe7ff6022ac2fc4f036388
khbsd <khb.dev@gmail.com>
2025-11-24 17:13:47 -0600

    cleanup: remove unused files and folder

--- js-scripts/whats_good.js
+++
@@ -1,9 +0,0 @@
-import { writeFile } from 'node:fs'
-import path from 'path'
-
-let string = "console.log('whats mcfuckin good cunts');";
-const data = new Uint8Array(Buffer.from(string));
-
-writeFile(path.normalize("./js-scripts/logging.js"), data, (err) => {
-    if (err) throw err;
-})
